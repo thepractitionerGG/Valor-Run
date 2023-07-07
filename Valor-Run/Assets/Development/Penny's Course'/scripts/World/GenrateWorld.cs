3f731e26a165e48b62db1fdcb03fb73929e3c67e
vikramsindhu <87515464+vikramsindhu@users.noreply.github.com>
2023-07-07 16:42:16 +0530

    code review done obstacle work started

--- Valor-Run/Assets/Development/Penny's Course'/scripts/World/GenrateWorld.cs
+++ Valor-Run/Assets/Development/Penny's Course'/scripts/World/GenrateWorld.cs
@@ -11,41 +11,6 @@ public class GenrateWorld : MonoBehaviour
     void Awake()
     {
         dummyTraveller = new GameObject("dummy");
-        
-        //for(int i = 0; i < 20; i++)
-        //{
-        //    GameObject p = Pools.singleton.GetRandom();
-        //    if (p == null)
-        //        return;
-
-        //    p.SetActive(true);
-        //    p.transform.position = dummyTraveller.transform.position;
-        //    p.transform.rotation = dummyTraveller.transform.rotation;
-        //    if (p.tag == "stairsUp")
-        //    {
-        //        dummyTraveller.transform.Translate(0,5,0);
-        //    }
-
-        //    else if (p.tag == "stairsDown")
-        //    {
-        //        dummyTraveller.transform.Translate(0, -5, 0);
-        //        p.transform.Rotate(new Vector3(0, 180, 0));
-        //        p.transform.position = dummyTraveller.transform.position;
-        //    }
-
-        //    else if (p.tag == "platformTSection")
-        //    {
-        //        if(Random.Range(0,2)==0)
-        //            dummyTraveller.transform.Rotate(new Vector3(0, 90, 0));
-        //        else
-        //            dummyTraveller.transform.Rotate(new Vector3(0, -90, 0));
-
-
-        //        dummyTraveller.transform.Translate(Vector3.forward * -10);
-        //    }
-
-        //    dummyTraveller.transform.Translate(Vector3.forward * -10);
-        //}
     }
 
     public static void RunDummy()
