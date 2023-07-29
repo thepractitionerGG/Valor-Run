bc057331e3c62829c13cb70f766557272e532809
vikramsindhu <87515464+vikramsindhu@users.noreply.github.com>
2023-07-29 09:42:10 +0530

    Obstacles edited arrow and elephant working, a collider is put on the obstacle object for detection

--- Valor-Run/Assets/Development/MainProject/scripts/WorldScripts/Deactivate.cs
+++ Valor-Run/Assets/Development/MainProject/scripts/WorldScripts/Deactivate.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Deactivate : MonoBehaviour
 {
     bool dScheduled = false;
-   
+    [SerializeField] GameObject[] moveableObjects;
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(GameObject g in moveableObjects)
+        {
+            g.SetActive(true);
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
         if (PlayerController.isDead) return;
-
+        foreach (GameObject g in moveableObjects)// add this condition 
+        {
+            g.SetActive(false);
+        }
         if (other.gameObject.tag == "Player" && !dScheduled)
         {
             Invoke("SetInactive", 2.0f);
