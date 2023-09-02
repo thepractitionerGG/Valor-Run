0ebfff57b6ee98eea2d3fa999bde430ff79290fe
Deep Bansal <dbansal@transfrvr.com>
2023-09-02 13:12:19 +0530

    achivement objets created

--- Valor-Run/Assets/Development/MainProject/scripts/Achivement/AchivementList.cs
+++ Valor-Run/Assets/Development/MainProject/scripts/Achivement/AchivementList.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AchivementList : MonoBehaviour
 {
-   
+   public AchivementItem[] achivementItemDetailsCoin;
+   public AchivementItem[] achivementItemDetailsSprint;
+   [SerializeField] GameObject _itemPrefab;
+   public void ListPopulator()
+   {
+
+   }
+    public enum AchivementState
+    {
+        Active,
+        Locked,
+        Completed
+    }
 }
